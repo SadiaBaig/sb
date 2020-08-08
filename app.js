eef500c5872f1acebda9cc3b59c911ff4efc9771
SadiaBaig <57152119+SadiaBaig@users.noreply.github.com>
2020-08-09 02:07:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+var myQuestions = [{
+        question: "your name",
+        answer: {
+            a: "ali",
+            b: "gh",
+            c: "sb"
+        },
+        correctAswer: "c"
+    },
+    {
+        question: "your age",
+        answer: {
+            a: 45,
+            b: 24,
+            c: 25
+        },
+        correctAswer: "c"
+    },
+]
